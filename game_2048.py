4a5d4d895d7805cfbc4d56dec6122084c7745d87
Nick Jalbert <nickjalbert@gmail.com>
2019-12-17 14:28:55 +0100

    initial implementation

--- game_2048.py
+++ game_2048.py
@@ -1,4 +1,5 @@
 import random
+import readchar
 
 
 class Game2048:
@@ -18,9 +19,10 @@ class Game2048:
             self.RIGHT: self._do_right,
             self.DOWN: self._do_down,
             self.LEFT: self._do_left,
-            }
+        }
         do_action[action]()
         self._set_random_position(2)
+        return self.board
 
     def reset(self):
         self.board = [None] * 16
@@ -33,8 +35,36 @@ class Game2048:
             f"{p[0]} {p[1]} {p[2]} {p[3]}\n"
             f"{p[4]} {p[5]} {p[6]} {p[7]}\n"
             f"{p[8]} {p[9]} {p[10]} {p[11]}\n"
-            f"{p[12]} {p[13]} {p[14]} {p[15]}\n"
+            f"{p[12]} {p[13]} {p[14]} {p[15]}\n\n"
         )
+
+    def run_manual_loop(self):
+        game.render_board()
+
+        moves = {
+            "w": Game2048.UP,
+            "A": Game2048.UP,
+            "d": Game2048.RIGHT,
+            "C": Game2048.RIGHT,
+            "s": Game2048.DOWN,
+            "B": Game2048.DOWN,
+            "a": Game2048.LEFT,
+            "D": Game2048.LEFT,
+        }
+        should_print = True
+        while True:
+            if should_print:
+                print("Move (w=UP, d=RIGHT, s=DOWN, a=LEFT, or arrows)?")
+                should_print = False
+            move = readchar.readchar()
+            if move in ["\x03", "\x04", "\x1a"]:
+                print("Exiting...")
+                break
+            if move not in moves:
+                continue
+            should_print = True
+            game.step(moves[move])
+            game.render_board()
 
     def _set_random_position(self, value):
         idxs = [i for i, val in enumerate(self.board) if val is None]
@@ -45,10 +75,10 @@ class Game2048:
         return idx
 
     def _do_up(self):
-        self._smush_by_index([0,4,8,12])
-        self._smush_by_index([1,5,9,13])
-        self._smush_by_index([2,6,10,14])
-        self._smush_by_index([3,7,11,15])
+        self._smush_by_index([0, 4, 8, 12])
+        self._smush_by_index([1, 5, 9, 13])
+        self._smush_by_index([2, 6, 10, 14])
+        self._smush_by_index([3, 7, 11, 15])
 
     def _do_right(self):
         self._smush_by_index([3, 2, 1, 0])
@@ -66,7 +96,7 @@ class Game2048:
         self._smush_by_index([0, 1, 2, 3])
         self._smush_by_index([4, 5, 6, 7])
         self._smush_by_index([8, 9, 10, 11])
-        self._smush_by_index([12, 13, 14, 5])
+        self._smush_by_index([12, 13, 14, 15])
 
     def _smush_by_index(self, idxs):
         smushed = self._smush_left([self.board[i] for i in idxs])
@@ -74,39 +104,19 @@ class Game2048:
             self.board[i] = smushed.pop(0)
 
     def _smush_left(self, col):
-        print(col)
-        new_col = []
-        curr_idx = 0
-        while curr_idx < len(col):
-            curr = col[curr_idx]
-            after_idx = curr_idx + 1
-            # at end of column
-            if after_idx >= len(col):
-                new_col.append(curr)
-                break
+        new_col = [v for v in col if v is not None]
+        i = 0
+        while i < len(new_col) - 1:
+            if new_col[i] == new_col[i + 1]:
+                new_col[i] *= 2
+                new_col.pop(i + 1)
+            i += 1
 
-            # None cannot be smushed
-            if curr is None:
-                curr_idx += 1
-                continue
-
-            after = col[after_idx]
-            if curr == after:
-                # Smush current and after
-                new_col.append(curr + after)
-                curr_idx = after_idx + 1
-            else:
-                # Keep current
-                new_col.append(curr)
-                curr_idx += 1
-
-        while len(new_col) < 4:
+        while len(new_col) < len(col):
             new_col.append(None)
         return new_col
 
 
 if __name__ == "__main__":
     game = Game2048()
-    game.render_board()
-    game.step(Game2048.UP)
-    game.render_board()
+    game.run_manual_loop()
