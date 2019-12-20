8173c32ace295b2d48dee6f56311e2da197bee7d
Andy Konwinski <andykonwinski@gmail.com>
2019-12-20 10:29:22 -0800

    rerun black on andy

--- andy_2048.py
+++ andy_2048.py
@@ -1,5 +1,4 @@
 # copied from https://colab.research.google.com/drive/1rp7kJrY-vkrCRziMJUatHL75dYVmlYv7
-from enum import Enum
 import random
 import pandas as pd
 import numpy as np
@@ -70,9 +69,9 @@ class BoardEnv:
                 # slide the piece in each row to the right (iterate check_spot to the right)
                 # stop when we get to the edge of the board or else a non-zero position.
                 while (
-                        check_col < self.width
-                        and check_col < stop_walls[curr_row]
-                        and curr_val != 0
+                    check_col < self.width
+                    and check_col < stop_walls[curr_row]
+                    and curr_val != 0
                 ):
                     check_val = state[curr_row, check_col]
                     # we will want to slide the cur position over top of this zero
@@ -115,9 +114,11 @@ class BoardEnv:
         self.state = new_board
         return new_board, reward, self.done
 
+
 def test_boardenv_random_direction():
     for _ in range(50):
         assert BoardEnv.random_direction() in [0, 1, 2, 3]
+
 
 def test_boardenv_init():
     board_width = random.randint(4, 10)
@@ -128,12 +129,14 @@ def test_boardenv_init():
         "BoardEnv initializing wrong num spots %s" % num_non_zero_spots
     )
 
+
 def test_boardenv_from_init_state():
     b = BoardEnv.from_init_state([[0, 0], [2, 0]])
     assert b.value == 0.0
     assert np.sum(b.state) == 2
     assert b.width == 2
     assert b.init_spots_filled == 1
+
 
 def test_board_env_step_one():
     # make sure the behavior is correct when a row is full of same values.
@@ -147,6 +150,7 @@ def test_board_env_step_one():
     state, reward, done = b.step(BoardEnv.RIGHT)
     assert state[0, 3] == 2.0 and state[2, 3] == 2.0
 
+
 def test_board_env_step_two():
     init_state = [
         [4.0, 2.0, 2.0, 4.0],
@@ -159,6 +163,7 @@ def test_board_env_step_two():
     assert state[0, 1] == 4.0
     assert state[0, 2] == 4.0
     assert state[0, 3] == 4.0
+
 
 def test_board_env_step_three():
     init_state = [
@@ -189,12 +194,13 @@ def test_boardenv_move_logic_four_in_a_row():
     assert reward >= 8
     assert state[0, 3] == 8, b.state
 
+
 def test_boardenv_done_logic():
     init_state = [
-        [16.0,  8.0, 16.0, 4.0],
-        [ 4.0,  2.0,  4.0, 8.0],
-        [32.0,  2.0, 32.0, 4.0],
-        [ 4.0, 16.0,  4.0, 8.0],
+        [16.0, 8.0, 16.0, 4.0],
+        [4.0, 2.0, 4.0, 8.0],
+        [32.0, 2.0, 32.0, 4.0],
+        [4.0, 16.0, 4.0, 8.0],
     ]
     b = BoardEnv().from_init_state(init_state)
     state, reward, done = b.step(BoardEnv.RIGHT)
@@ -210,6 +216,7 @@ def test_boardenv_done_logic():
     assert done, state
     assert reward == 4
 
+
 def test_boardenv_move_logic_three_in_a_row():
     # make sure the behavior is correct when 3 elts are same in a row
     init_state = [
@@ -223,6 +230,7 @@ def test_boardenv_move_logic_three_in_a_row():
     state, reward, done = b.step(BoardEnv.DOWN)
     assert reward == 4
     assert state[3, 1] == 4 and state[2, 1] == 2, b.state
+
 
 def test_boardenv_fill_on_move_logic():
     # make sure a new piece is added that is either a 2 or a 4
