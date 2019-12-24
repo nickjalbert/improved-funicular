ba47c36ff7b0f0936ad1dbc31d780c425851f942
Nick Jalbert <nickjalbert@gmail.com>
2019-12-24 16:58:17 +0000

    Updates to greedy strats

--- do_stats.py
+++ do_stats.py
@@ -62,9 +62,8 @@ def try_random(cls):
 
 def try_down_left(cls):
     def down_left_fn(board):
-        test_game = cls()
-        test_game.set_board(board)
-        valid_actions = [a for (a, r) in test_game.get_valid_actions()]
+        action_rewards = Nick2048.get_valid_actions_from_board(board)
+        valid_actions = [a for (a, r, b) in action_rewards]
         down_left = [cls.DOWN, cls.LEFT]
         up_right = [cls.UP, cls.RIGHT]
         random.shuffle(down_left)
@@ -78,47 +77,109 @@ def try_down_left(cls):
     do_trials(cls, down_left_fn)
 
 
+def try_fixed_action_order(cls):
+    # Always choose actions in a particular order if they are valid
+    def fixed_fn(board):
+        ACTION_ORDER = [cls.DOWN, cls.LEFT, cls.UP, cls.RIGHT]
+        actions = Nick2048.get_valid_actions_from_board(board)
+        actions = [a for (a, r, b) in actions]
+        assert len(actions) > 0, "No actions available"
+        for action in ACTION_ORDER:
+            if action in actions:
+                return action
+        assert False, "Could not find action"
+
+    fixed_fn.info = "Fixed order strategy"
+    do_trials(cls, fixed_fn)
+
+
 def try_greedy(cls):
+    # Greedy, break ties randomly
     def greedy_fn(board):
-        test_game = cls()
-        test_game.set_board(board)
-        action_rewards = test_game.get_valid_actions_by_reward()
-        assert len(action_rewards) > 0, "No actions available"
-        return action_rewards[0][0]
+        actions = Nick2048.get_valid_actions_by_reward_from_board(board)
+        assert len(actions) > 0, "No actions available"
+        return actions[0][0]
 
     greedy_fn.info = "Greedy strategy"
     do_trials(cls, greedy_fn)
 
 
+def try_greedy_fixed_order(cls):
+    ORDER = [cls.UP, cls.DOWN, cls.LEFT, cls.RIGHT]
+    random.shuffle(ORDER)
+    # Greedy, break ties in a fixed order
+    def greedy_fixed_order_fn(board):
+        actions = Nick2048.get_valid_actions_by_reward_from_board(board)
+        top_reward = actions[0][1]
+        equiv_rewards = [a for (a, r, b) in actions if r >= top_reward]
+        assert len(equiv_rewards) > 0, "No actions available"
+        for action in ORDER:
+            if action in equiv_rewards:
+                return action
+        assert False
+
+    greedy_fixed_order_fn.info = "Greedy strategy with fixed preference"
+    do_trials(cls, greedy_fixed_order_fn)
+
+
 def try_down_left_greedy(cls):
     # pick best of {down, left} if available, otherwise best of {up, right}
     def down_left_greedy_fn(board):
-        test_game = cls()
-        test_game.set_board(board)
-        action_rewards = test_game.get_valid_actions_by_reward()
-        assert len(action_rewards) > 0, "No actions available"
-        for action, reward in action_rewards:
+        actions = Nick2048.get_valid_actions_by_reward_from_board(board)
+        assert len(actions) > 0, "No actions available"
+        for action, reward, board in actions:
             if action in [cls.DOWN, cls.LEFT]:
                 return action
-        return action_rewards[0][0]
+        return actions[0][0]
 
     down_left_greedy_fn.info = "Down left greedy strategy"
     do_trials(cls, down_left_greedy_fn)
 
 
+def try_max_space_then_greedy(cls):
+    def max_space_then_greedy_fn(board):
+        actions = Nick2048.get_valid_actions_from_board(board)
+        assert len(actions) > 0, "No actions available"
+
+        def key_fn(el):
+            return (-1 * el[2].count(0), -1 * el[1])
+
+        sorted_actions = sorted(actions, key=key_fn)
+        top_action, top_reward, top_board = sorted_actions[0]
+        top_action_zeros = top_board.count(0)
+        for (a, r, b) in sorted_actions:
+            assert b.count(0) <= top_action_zeros
+        equiv_actions = [
+            a
+            for (a, r, b) in sorted_actions
+            if b.count(0) >= top_action_zeros and r >= top_reward
+        ]
+        ORDER = [cls.UP, cls.DOWN, cls.LEFT, cls.RIGHT]
+        for action in ORDER:
+            if action in equiv_actions:
+                return action
+        assert False
+
+    max_space_then_greedy_fn.info = "Max space then greedy"
+    do_trials(cls, max_space_then_greedy_fn)
+
+
 def do_stats():
     print(f"\nRunning {TRIALS} trials with Nick impl to test each strategy\n")
-    try_only_go_right(Nick2048)
-    try_random(Nick2048)
+    # try_only_go_right(Nick2048)
+    # try_random(Nick2048)
     try_down_left(Nick2048)
+    # try_fixed_action_order(Nick2048)
     try_greedy(Nick2048)
-    try_down_left_greedy(Nick2048)
+    try_greedy_fixed_order(Nick2048)
+    # try_down_left_greedy(Nick2048)
+    try_max_space_then_greedy(Nick2048)
     print(f"\nRunning {TRIALS} trials with Andy impl to test each strategy\n")
-    try_only_go_right(Andy2048)
-    try_random(Andy2048)
-    try_down_left(Andy2048)
-    try_greedy(Andy2048)
-    try_down_left_greedy(Andy2048)
+    # try_only_go_right(Andy2048)
+    # try_random(Andy2048)
+    # try_down_left(Andy2048)
+    # try_greedy(Andy2048)
+    # try_down_left_greedy(Andy2048)
 
 
 if __name__ == "__main__":
