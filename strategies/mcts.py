358dc53923a458acc39ac67b0016aef969d12ba2
Andy Konwinski <andykonwinski@gmail.com>
2020-01-09 11:56:23 -0800

    small updates to MCTS per RL textbook, Sutton and Bartow

--- strategies/mcts.py
+++ strategies/mcts.py
@@ -5,38 +5,42 @@ from strategies.utility import do_trials
 
 # globals so that we'll keep value function state across training rollouts
 n = defaultdict(int)  # tuple(board), action -> count of visits
-sum_ret = defaultdict(int)  # tuple(board), action -> expected return val
+sum_ret = defaultdict(lambda: 1)  # tuple(board), action -> expected return val
 
 
-def train_mcts(cls, next_action, num_rollouts=100000):
-    epsilon = 0.1
-    discount_rate = 0.95
+def train_mcts(cls, next_action, num_rollouts=100000, epsilon=0.1, discount_rate=0.95):
     print("training MCTS value function")
     for rollout_num in range(num_rollouts):
+        prob_rand_action = min(1, 1000 * epsilon / (rollout_num + 1))
         if rollout_num % 2000 == 0:
             print("training rollout num %s" % rollout_num)
+            print("epsilon: %0.2f" % prob_rand_action)
+            print("len of sum_ret dict: %s" % len(sum_ret))
         game = cls()
         curr_board, score, done = game.get_state()
-        states, actions, rewards = [], [], []
+        states, actions, rewards, is_duplicate = [], [], [], []
+        state_action_pairs = set()
         while not done:
             assert curr_board == game.board
-            if random.random() < epsilon:
+            if random.random() < prob_rand_action:
                 action = cls.random_direction()
             else:
                 action = next_action(curr_board)
             states.append(curr_board)
-            curr_board, reward, done = game.step(action)
             actions.append(action)
+            is_duplicate.append((tuple(curr_board), action) in state_action_pairs)
+            curr_board, reward, done = game.step(action)
             rewards.append(reward)
 
         # calculate returns (i.e., discounted future rewards) using bellman backups for the rollout just completed
         returns = [0] * len(rewards)
         returns[-1] = rewards[-1]
         for i in range(len(rewards) - 2, -1, -1):
-            n[(tuple(states[i]), actions[i])] += 1
-            sum_ret[(tuple(states[i]), actions[i])] += (
-                rewards[i] + discount_rate * returns[i + 1]
-            )
+            if not is_duplicate[i]:
+                n[(tuple(states[i]), actions[i])] += 1
+                sum_ret[(tuple(states[i]), actions[i])] += (
+                    rewards[i] + discount_rate * returns[i + 1]
+                )
 
 
 def try_mcts(cls, trial_count):
