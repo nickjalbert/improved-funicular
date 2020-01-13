e9d37fcf05811434ed92fded9469f0d62bc8b246
Andy Konwinski <andykonwinski@gmail.com>
2020-01-13 12:27:01 -0800

    run ray PPO algo on 2 cpus, 1000 iterations, save to MLflow

--- ray_algo.py
+++ ray_algo.py
@@ -9,8 +9,10 @@ This example shows:
 You can visualize experiment results in ~/ray_results using TensorBoard.
 """
 
+import mlflow
 import numpy as np
 import gym
+from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.models.tf.fcnet_v2 import FullyConnectedNetwork
@@ -20,32 +22,10 @@ import ray
 from ray import tune
 from ray.rllib.utils import try_import_tf
 from ray.tune import grid_search
+from ray.tune.logger import pretty_print
 
 tf = try_import_tf()
 
-#class SimpleCorridor(gym.Env):
-#    """Example of a custom env in which you have to walk down a corridor.
-#    You can configure the length of the corridor via the env config."""
-#
-#    def __init__(self, config):
-#        self.end_pos = config["corridor_length"]
-#        self.cur_pos = 0
-#        self.action_space = Discrete(2)
-#        self.observation_space = Box(
-#            0.0, self.end_pos, shape=(1,), dtype=np.float32)
-#
-#    def reset(self):
-#        self.cur_pos = 0
-#        return [self.cur_pos]
-#
-#    def step(self, action):
-#        assert action in [0, 1], action
-#        if action == 0 and self.cur_pos > 0:
-#            self.cur_pos -= 1
-#        elif action == 1:
-#            self.cur_pos += 1
-#        done = self.cur_pos >= self.end_pos
-#        return [self.cur_pos], 1 if done else 0, done, {}
 
 
 class CustomModel(TFModelV2):
@@ -68,22 +48,43 @@ class CustomModel(TFModelV2):
 
 if __name__ == "__main__":
     # Can also register the env creator function explicitly with:
-    # register_env("corridor", lambda config: SimpleCorridor(config))
-    ray.init()
-    ModelCatalog.register_custom_model("my_model", CustomModel)
-    tune.run(
-        "PPO",
-        stop={
-            "timesteps_total": 30000,
-        },
-        config={
-            "env": Nick2048,  # or "corridor" if registered above
-            "model": {
-                "custom_model": "my_model",
-            },
-            "vf_share_layers": True,
-            "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
-            "num_workers": 2,  # parallelism
-            #"env_config": {},
-        },
-    )
+    #register_env("2048-v0", lambda config: Nick2048(config))
+    with mlflow.start_run():
+        ray.init(num_cpus=2)
+        ModelCatalog.register_custom_model("my_model", CustomModel)
+
+        config = {}
+        config['num_workers'] = 2
+        config['num_sgd_iter'] = 10
+        config['sgd_minibatch_size'] = 512
+        config['model'] = {'fcnet_hiddens': [10, 10]}
+        config['num_cpus_per_worker'] = 1
+        mlflow.log_params(config)
+
+        full_config = DEFAULT_CONFIG.copy()
+        for k, v in config.items():
+            full_config[k] = v
+
+        agent = PPOTrainer(full_config, env=Nick2048)
+        for i in range(1000):
+            res = agent.train()
+            print(res["episode_reward_mean"])
+            #mlflow.log_metrics(res) #<-- MLflow can't handle nested dictionaries of metrics.
+            mlflow.log_metric("episode_reward_mean", res["episode_reward_mean"], step=i)
+
+        #tune.run(
+        #    "PPO",
+        #    stop={
+        #        "timesteps_total": 30000,
+        #    },
+        #    config={
+        #        "env": Nick2048,  # or "corridor" if registered above
+        #        "model": {
+        #            "custom_model": "my_model",
+        #        },
+        #        "vf_share_layers": True,
+        #        "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
+        #        "num_workers": 3,  # parallelism
+        #        #"env_config": {},
+        #    },
+        #)
