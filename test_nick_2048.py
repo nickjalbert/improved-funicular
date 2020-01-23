d39648a46236562335e57852158021e0eab0ead9
Nick Jalbert <nickjalbert@gmail.com>
2020-01-23 11:31:08 +0100

    Tests for rotation and reflection

--- test_nick_2048.py
+++ test_nick_2048.py
@@ -167,3 +167,55 @@ def test_get_valid_actions_by_reward():
     assert action_rewards[3] in up_down
     for (a, r, b) in Nick2048.get_valid_actions_by_reward_from_board(board):
         assert (a, r) in action_rewards
+
+
+def test_rotate_board():
+    # 2 0 4 8
+    # 2 0 0 0
+    # 4 4 0 0
+    # 0 0 0 8
+    board = tuple([2, 0, 4, 8, 2, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 8])
+    result_90 = Nick2048.rotate_board_right(board)
+    # 0 4 2 2
+    # 0 4 0 0
+    # 0 0 0 4
+    # 8 0 0 8
+    assert result_90 == tuple([0, 4, 2, 2, 0, 4, 0, 0, 0, 0, 0, 4, 8, 0, 0, 8])
+    result_180 = Nick2048.rotate_board_right(result_90)
+    # 8 0 0 0
+    # 0 0 4 4
+    # 0 0 0 2
+    # 8 4 0 2
+    assert result_180 == tuple([8, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 2, 8, 4, 0, 2])
+    result_270 = Nick2048.rotate_board_right(result_180)
+    # 8 0 0 8
+    # 4 0 0 0
+    # 0 0 4 0
+    # 2 2 4 0
+    assert result_270 == tuple([8, 0, 0, 8, 4, 0, 0, 0, 0, 0, 4, 0, 2, 2, 4, 0])
+    result_360 = Nick2048.rotate_board_right(result_270)
+    assert result_360 == board
+
+
+def test_reflect_board():
+    # 2 0 4 8
+    # 2 0 0 0
+    # 4 4 0 0
+    # 0 0 0 8
+    board = tuple([2, 0, 4, 8, 2, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 8])
+    reflected_y_1 = Nick2048.reflect_board_across_y(board)
+    # 8 4 0 2
+    # 0 0 0 2
+    # 0 0 4 4
+    # 8 0 0 0
+    assert reflected_y_1 == tuple([8, 4, 0, 2, 0, 0, 0, 2, 0, 0, 4, 4, 8, 0, 0, 0])
+    reflected_y_2 = Nick2048.reflect_board_across_y(reflected_y_1)
+    assert reflected_y_2 == board
+    reflected_x_1 = Nick2048.reflect_board_across_x(board)
+    # 0 0 0 8
+    # 4 4 0 0
+    # 2 0 0 0
+    # 2 0 4 8
+    assert reflected_x_1 == tuple([0, 0, 0, 8, 4, 4, 0, 0, 2, 0, 0, 0, 2, 0, 4, 8])
+    reflected_x_2 = Nick2048.reflect_board_across_x(reflected_x_1)
+    assert reflected_x_2 == board
