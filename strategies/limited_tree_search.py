9f87c84b3a6dc1cd7d7ab6de1f19f9e9e08ef82b
Nick Jalbert <nickjalbert@gmail.com>
2020-01-27 18:53:04 +0100

    wip

--- strategies/limited_tree_search.py
+++ strategies/limited_tree_search.py
@@ -1,27 +1,42 @@
 import statistics
 from strategies.utility import do_trials
 
-ROLLOUT_DEPTH_LIMIT = 15
-ROLLOUTS_PER_MOVE = 20
+ROLLOUT_DEPTH_LIMIT = 10
+ROLLOUTS_PER_MOVE = 100
 
 
 def best_next_move_from_random_rollouts(cls, board):
     action_rewards = cls.get_valid_actions_from_board(board)
     valid_actions = [a for (a, r, b) in action_rewards]
-    avg_score = {}
     test_game = cls()
+    best_rollouts = {}
     for action in valid_actions:
-        scores = []
+        best_rollout_score = 0
         for i in range(ROLLOUTS_PER_MOVE):
+            rollout = []
             test_game.score = 0
             test_game.set_board(board)
             test_game.step(action)
+            rollout.append(action)
             depth = 0
-            while not test_game.done and depth <= ROLLOUT_DEPTH_LIMIT:
+            while not test_game.done and depth < ROLLOUT_DEPTH_LIMIT:
                 old_board = test_game.board
-                test_game.step(test_game.action_space.sample())
+                tmp_action = test_game.action_space.sample()
+                test_game.step(tmp_action)
                 if test_game.board != old_board:
+                    rollout.append(tmp_action)
                     depth += 1
+            if test_game.score >= best_rollout_score:
+                best_rollout_score = test_game.score
+                best_rollouts[action] = rollout
+    avg_score = {}
+    for action, rollout in best_rollouts.items():
+        scores = []
+        for i in range(ROLLOUTS_PER_MOVE):
+            test_game.set_board(board)
+            test_game.score = 0
+            for a in rollout:
+                test_game.step(a)
             scores.append(test_game.score)
         avg_score[action] = statistics.mean(scores)
     action = max(avg_score.items(), key=lambda x: x[1])[0]
